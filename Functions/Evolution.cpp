feba75416578a36a9a4b1ccda8d1cc66f43e4dfb
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2023-10-10 13:34:09 +0200

    removed all comments from the two privous commit. Those comments were there to debug the code

---
+++ Functions/Evolution.cpp
@@ -0,0 +1,47 @@
+#include "../Headers/Genome.hpp"
+
+
+void genome::Evolution(double evolve)
+{   
+    selected_node = input[0];
+    for (int i=0 ; i< n ; i++) gn[i].nm_up = 0;
+    gn[selected_node].weights = evolve;
+    
+    
+    for (int i=0 ; i<gn[selected_node].nghbrs.size() ; i++)
+    {
+        int node2 = gn[selected_node].nghbrs[i];
+        double VV = 0;   
+        
+        for (int GH=0 ; GH < gn[node2].Connected.size(); GH++)
+        {
+            int gh = gn[node2].Connected[GH];
+            VV += double(gn[gh].weights * adjac[gh][node2]);
+        }
+        gn[node2].weights = The_Function(VV);
+        updater (node2);
+    }
+
+}
+
+void genome::updater (int nodex)
+{   
+    if (gn[nodex].nm_up < gn[nodex].slf_cntrl)
+    {
+        for (int i=0 ; i<gn[nodex].nghbrs.size() ; i++)
+        {   
+            double values = 0;
+            int node4 = gn[nodex].nghbrs[i];
+
+            for (int GH=0 ; GH < gn[node4].Connected.size(); GH++)
+            {
+                int gh = gn[node4].Connected[GH];
+                values += double(gn[gh].weights * adjac[gh][node4]);
+            }
+
+            gn[node4].weights = The_Function(values);
+            gn[node4].nm_up ++;
+            updater (node4);
+        }
+    }
+}
