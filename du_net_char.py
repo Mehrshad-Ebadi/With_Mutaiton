a9ee211846002d5406a5f6d020ddcf48c5bb096c
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2023-10-20 12:00:23 +0200

    I forgot to add the du_net_char file, now this is the best version

---
+++ du_net_char.py
@@ -0,0 +1,56 @@
+import numpy as np
+import networkx as nx
+from igraph import Graph
+
+step = 0
+number_networks = 0
+
+with open('./Outputs/temp.txt', 'r') as file:
+    step = float(file.readline().strip())
+    number_networks = int(file.readline().strip())
+
+exten = '.txt'
+Results = './Results_du/Net_du_'
+save_path = './Outputs/00_du_Net_analysis.txt'
+
+for I in range (0, number_networks):
+
+    i = str(I)
+    file_path = Results + i + exten
+    
+    adjacency_matrix = np.loadtxt(file_path)
+    adjacency_matrix = np.abs(adjacency_matrix)
+    G = nx.DiGraph(adjacency_matrix)
+    num_nodes = len(G.nodes())
+    num_edges = len(G.edges())
+    
+    average_in_degree = round(sum(dict(G.in_degree(weight='weight')).values()) / num_nodes, 3)
+    average_out_degree = round(sum(dict(G.out_degree(weight='weight')).values()) / num_nodes, 3)
+
+    try:
+        average_degree = round(sum(dict(G.degree()).values()) / num_nodes, 3)
+    except:
+        average_degree = None
+    
+    try:
+        clustering_coefficient = round(nx.average_clustering(G), 3)
+    except:
+        clustering_coefficient = None
+    
+    try:
+        diameter = round(nx.diameter(G), 3)
+
+    except:
+        diameter=None
+        try:
+            diameter = round(nx.diameter(G.to_undirected(), weight='weight'), 3)
+        except:
+            diameter=None
+        
+    
+    with open(save_path, 'a') as file:
+           
+        file.write(f"{step},{I},{average_in_degree},{average_out_degree},{diameter},{clustering_coefficient}")
+    del G
+    file = open(save_path, 'a')
+    file.write('\n')
