5cb025a30f39313d6ea61d2f4a03dd9ab8881d7d
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2023-10-23 16:33:27 +0200

    copied results untill 9999

---
+++ submit_mutation.sh
@@ -0,0 +1,11 @@
+rm Results/*
+rm Results_du/*
+echo "removed the old population and start copying..."
+
+cp ./Population_pool/Results/Net_{0..9999}.txt ./Results
+cp ./Population_pool/Results_du/Net_du_{0..9999}.txt ./Results_du
+echo "copying done!"
+echo "the package started!"
+./Exe/Final.out
+
+echo "simulation finished!"
