fa6d6bd5f6995ec644cb737d656cf108878a8dca
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2023-11-06 02:09:16 +0100

    full of errors and I dont know what is the problem

---
+++ Headers/DU_net_gene.hpp
@@ -0,0 +1,34 @@
+#include "./duplicated.hpp"
+
+class dNet
+{
+    public:
+        
+        dupli* du;
+        double** du_adjac;
+        int du_II;
+        int du_UU;
+        int * du_output;
+        int * du_input;
+        double fitness;
+        double XX;
+        bool living;
+        int nn;
+        
+        dNet(){};
+        
+    ~dNet()
+            
+    {
+        delete []du_output;
+        delete []du_input;
+
+        for (int i = 0; i < nn ; ++i) 
+        {
+            delete[] du_adjac[i];
+        }
+        delete [] du_adjac;
+
+        delete [] du;
+    }
+};
---
+++ Headers/Net_gene.hpp
@@ -0,0 +1,31 @@
+#include "./Gene.hpp"
+
+class Net{
+public:
+    gene* gn;
+    double** adjac;
+    int II;
+    int UU;
+    int * output;
+    int * input;
+    double fitness;
+    double xx;
+    bool living;
+    int n;
+    
+    Net(){};
+
+    ~Net()
+    {
+        delete []output;
+        delete []input;
+
+        for (int i = 0; i < n; ++i) 
+        {
+            delete[] adjac[i];
+        }
+        delete [] adjac;
+
+        delete [] gn;
+    }
+};
