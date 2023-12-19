91e3d916b18ba59717b93272bf57fb7b4c1ee3dc
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2023-12-19 13:22:25 +0100

    Main is modified as well!

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -2,43 +2,58 @@
 
 int main ()
 {
+    ifstream file("./input/parameters.csv"); // Replace with your CSV file path
+    string line;
+    int lineCount = 0;
+
+    if (file.is_open()) {
+        while (getline(file, line))
+            lineCount++;
+
+        file.close(); // Always close the file when you're done with it.
+        cout << "Number of lines in the file: " << lineCount/6 << endl;
+    }
+
     ifstream Predifined ("./input/parameters.csv");
     int N = 20;
-    int st;
-    int Nn_net;
-    float Mute_R, per_iso;
+    int st = 10000;
+    int Nn_net = 1000;
+    float Mute_R;
     double  env_ref;
     int envi_func;
-    Predifined >> Mute_R;
+    
 
-    for (int a=0 ; a<4 ; a++)
+    for (int jk=0 ; jk<lineCount ; jk++)
     {
-        envi_func = a;
+        Predifined >> Mute_R;
 
-        for (int s=0 ; s<5 ; s++)
+        for (int a=0 ; a<4 ; a++)
         {
-            switch (s)
-            {
-                case 0:
-                    env_ref = 0;
-            
-                case 1:
-                    env_ref = 0.1;
-                
-                case 2:
-                    env_ref = 0.5;
-                
-                case 3:
-                    env_ref = 1.0;
-                
-                case 4:
-                    env_ref = 1.2;
-            }
+            envi_func = a;
 
-            st = 10000;
-            Nn_net = 1000;
-            genome GM;
-            GM.Setting_initial_values(N, st, Nn_net, Mute_R, env_ref, envi_func);
+            for (int s=0 ; s<5 ; s++)
+            {
+                switch (s)
+                {
+                    case 0:
+                        env_ref = 0;
+                
+                    case 1:
+                        env_ref = 0.1;
+                    
+                    case 2:
+                        env_ref = 0.5;
+                    
+                    case 3:
+                        env_ref = 1.0;
+                    
+                    case 4:
+                        env_ref = 1.2;
+                }
+
+                genome GM;
+                GM.Setting_initial_values(N, st, Nn_net, Mute_R, env_ref, envi_func);
+            }
         }
     }
 }
