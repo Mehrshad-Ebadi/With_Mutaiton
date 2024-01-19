1066fef240ac5f6cc5a0bfd1c12f2194f186a9b1
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2024-01-19 14:19:49 +0100

    done!

---
+++ violin_average.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = list()
+b = list()
+
+for i in range (0,10):
+    addres = './Outputs/st_10000,nn_10000,mu_0.000100,ref_env_=0.200000/runnum_'
+    addres = addres + str(i)
+    addres = addres + '/Alive.txt'
+    # Load data from the "Alive.txt" file
+    alive_data = np.loadtxt(addres)
+    
+    # Load data from the "du_Alive.txt" file
+    du_alive_file_path = './Outputs/du_Alive.txt'
+    du_alive_data = np.loadtxt(du_alive_file_path)
+    a += alive_data
+    b += du_alive_data
+
+a = a / 10
+b = b / 10
+# Organize the data into groups of 500 steps for both datasets
+alive_grouped_data = [alive_data[i:i + 500, 1] for i in range(0, len(alive_data), 500)]
+du_alive_grouped_data = [du_alive_data[i:i + 500, 1] for i in range(0, len(du_alive_data), 500)]
+
+# Set the width of each violin
+width = 0.4
+
+# Create a combined violin plot for both datasets
+fig, ax = plt.subplots()
+
+# Plot for "Alive.txt"
+ax.violinplot(alive_grouped_data, positions=np.arange(1, len(alive_grouped_data) + 1) - width/2, showmeans=False, showmedians=True, widths=width)
+
+# Plot for "du_Alive.txt"
+ax.violinplot(du_alive_grouped_data, positions=np.arange(1, len(du_alive_grouped_data) + 1) + width/2, showmeans=False, showmedians=True, widths=width)
+
+# Customize labels and title
+ticks_positions = np.arange(1, len(alive_grouped_data) + 1)
+ticks_labels = [f'{int(i*500/1000)}k' for i in ticks_positions]
+plt.tick_params(axis='x', which='both', labelsize=8)
+plt.xticks(ticks_positions, ticks_labels, rotation='vertical')  # Set rotation to 'vertical'
+plt.xlabel('Time steps')
+plt.ylabel('S')
+#plt.title('Violin Plot for S')
+
+# Show the plot
+plt.savefig('./diagrams/violin.png', dpi=700)
