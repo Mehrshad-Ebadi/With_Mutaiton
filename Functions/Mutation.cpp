15cb8d94de9c12bf52203613d8cb5a1599b03381
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2024-04-03 11:36:09 +0200

    New changes are adapted

--- Functions/Mutation.cpp
+++ Functions/Mutation.cpp
@@ -14,7 +14,7 @@ void genome::Mutation(int temp_net)
             int f = ran2(&iseed) * ne[s].gn[i].nghbrs.size();
             f = ne[s].gn[i].nghbrs[f];
             ne[s].nm_mutation++;
-            ne[s].adjac [i][f] = gasdev(&iseed);
+            ne[s].adjac [i][f] = ne[s].adjac [i][f] + gasdev(&iseed);
         }
 
     }
--- Functions/du_mutation.cpp
+++ Functions/du_mutation.cpp
@@ -16,42 +16,8 @@ void genome::du_Mutation(int temp_net)
         {
             int f = ran2(&iseed) * dn[s].du[i].nghbrs.size();
             f = dn[s].du[i].nghbrs[f];
-            dn[s].du_adjac [i][f] = gasdev(&iseed);
+            dn[s].du_adjac [i][f] = dn[s].du_adjac [i][f] + gasdev(&iseed);
             dn[s].nm_mutation ++;
         }
-
-        if (ran2(&iseed) < rwrng_nw_cnnctn && dn[s].du[i].nghbrs.size() < nn)
-        {
-            bool temmpy = true;
-            while (temmpy)
-            {
-                int f = ran2(&iseed) * nn;
-                if (dn[s].du_adjac [i][f] == 0)
-                {
-                    du_connect(i, f, gasdev(&iseed), s);
-                    du_specefication(s);
-                    temmpy = false;
-                    dn[s].nm_mutation ++;
-                }                
-            }
-        }
-
-        if (ran2(&iseed) < rwrng_dl_cnnctn && dn[s].du[i].nghbrs.size() > 0)
-        {
-            int f = ran2(&iseed) * dn[s].du[i].nghbrs.size();
-            int FE = dn[s].du[i].nghbrs[f];
-            dn[s].du[i].nghbrs.erase(dn[s].du[i].nghbrs.begin() + f);
-            dn[s].du_adjac[i][FE] = 0;
-            dn[s].edges --;
-            dn[s].du[i].dg_out --;
-
-            auto it = find (dn[s].du[FE].Connected.begin(), dn[s].du[FE].Connected.end(), i);
-            
-            dn[s].du[FE].Connected.erase(it);
-            dn[s].du[FE].dg_in --;
-            dn[s].nm_mutation ++;
-            
-            du_specefication(s);
-        }
-    }    
+    }
 }
