195255b9534ea9424cc126f4b4ac4d6b5fcdb4fc
Mehrshad Ebadi <ebadi.mehrshad@gmail.com>
2024-12-02 09:39:22 +0100

    Update environment_changes.cpp

--- Functions/environment_changes.cpp
+++ Functions/environment_changes.cpp
@@ -4,7 +4,7 @@ double genome::Environment_li(int inii)
 {
 
     //linear environment:
-    double evolve = (static_cast<double>(inii * len_step)) + first_input ; 
+    double evolve = (static_cast<double>(inii * len_step)) + first_input ;
     return evolve;
 }
 
@@ -21,16 +21,16 @@ double genome::Environment_Ga()
     }
     return evolve;
 }
-    
+
 
 double genome::Environment_uniform()
 {
     double evolve = 2.0;
     while (abs (evolve) > 1.9)
     {
-    	evolve = first_input + ((last_input - first_input) * ran2(&iseed));
+        evolve = first_input + ((last_input - first_input) * ran2(&iseed));
 
-    }	
+    }
 
     return evolve;
 }
@@ -41,3 +41,4 @@ double genome::Environment_neg(int inii)
     double evolve = first_input + (static_cast<double>(inii * len_step));
     return evolve;
 }
+~       
